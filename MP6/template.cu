9cb6f88a00f671dba3dc2c649f160256789857b3
jackruda21 <jcruda2@illinois.edu>
2023-04-08 20:27:29 -0500

    MP6 started, Eq1 written, CDF func to be checked

--- MP6/template.cu
+++ MP6/template.cu
@@ -6,14 +6,91 @@
 #define BLOCK_SIZE 16
 
 //@@ insert code here
-__global__ void histEq(float *input, int height, int width){
+__global__ void histEq1(float *input, float *output, float *histo, int height, int width){
   //initialize shared data
-  __shared__ blockHistogram[HISTOGRAM_LENGTH];
-  __shared__ blockGrayImg[BLOCK_SIZE*BLOCK_SIZE];
+  __shared__ int blockHistogram[HISTOGRAM_LENGTH];
+  __shared__ unsigned char blockGrayImg[BLOCK_SIZE*BLOCK_SIZE];
 
-  //Set index
-  int idx = 
+  //Set indices
+  int ix = blockIdx.x * blockDim.x + threadIdx.x;
+  int iy = blockIdx.y * blockDim.y + threadIdx.y;
+  int idx = iy * width + ix;
+  int block_idx = threadIdx.y * BLOCK_SIZE + threadIdx.x;
 
+  // Set shared block w/ grayscale values
+  unsigned char r = (unsigned char) 255 * input[3*idx];
+  unsigned char g = (unsigned char) 255 * input[3*idx + 1];
+  unsigned char b = (unsigned char) 255 * input[3*idx + 2];
+  blockGrayImg[block_idx] = (unsigned char) (0.21*r + 0.71*g + 0.07*b)
+
+  //init histogram to 0
+  blockHistogram[block_idx] = 0;
+
+  //create local shared histogram based on image
+  atomicAdd( &(blockHistogram[blockGrayImg[block_idx]]), 1);
+  __syncthreads();
+
+  //add to global histogram
+  atomicAdd( &histo[block_idx] , blockHistogram[block_idx]);
+}
+
+//kernel function to compute CDF, DOUBLE CHECK THIS!!!
+__global__ void computeCDF(float *hist, float *CDF, int len){
+
+  __shared__ float partialSum[HISTOGRAM_LENGTH];
+  //set up shared mem
+  int i = 2*(blockIdx.x*blockDim.x + threadIdx.x);
+  
+  if (i < len){
+    partialSum[2*threadIdx.x] = input[i];
+  }
+  else{
+    partialSum[2*threadIdx.x] = 0;
+  }
+
+  if (i+1 < len){
+    partialSum[2*threadIdx.x+1] = input[i+1];
+  }
+  else{
+    partialSum[2*threadIdx.x+1] = 0;
+  }
+  
+  //Brent-Kung Algo
+  //Parallel Scan
+  int stride = 1;
+  int index = 0;
+  while(stride < HISTOGRAM_LENGTH) {
+    __syncthreads();
+    index = (threadIdx.x+1)*stride*2 - 1;
+    if(index < HISTOGRAM_LENGTH && (index-stride) >= 0)
+      partialSum[index] += partialSum[index-stride];
+    stride = stride*2;
+  }
+
+  //Post Scan
+  stride = BLOCK_SIZE/2;
+  while(stride > 0) {
+    __syncthreads();
+    index = (threadIdx.x+1)*stride*2 - 1;
+    if ((index+stride) < HISTOGRAM_LENGTH)
+      partialSum[index+stride] += partialSum[index];
+    stride = stride / 2;
+  }
+  //REQUIRED bc threads were writing to output before calculation was finished
+  __syncthreads();
+  
+  //set output
+  if(i<len)
+    output[i] = partialSum[2*threadIdx.x];
+  
+  if(i+1<len)
+   output[i+1] = partialSum[2*threadIdx.x+1];
+  
+
+  //set intermediates if they're not NULL
+  if(threadIdx.x == 0 && inter != NULL){
+    inter[blockIdx.x] = partialSum[HISTOGRAM_LENGTH-1];
+  }
 }
 
 int main(int argc, char **argv) {
@@ -28,6 +105,11 @@ int main(int argc, char **argv) {
   const char *inputImageFile;
 
   //@@ Insert more code here
+  float *deviceInput;
+  float *deviceOutput;
+  float *deviceHisto;
+  float *deviceCDF;
+
 
   args = wbArg_read(argc, argv); /* parse the input arguments */
 
@@ -44,6 +126,29 @@ int main(int argc, char **argv) {
   wbTime_stop(Generic, "Importing data and creating memory on host");
 
   //@@ insert code here
+
+  //alloc device mem
+  cudaMalloc((void**)&deviceInput, imageWidth*imageHeight*imageChannels*sizeof(float));
+  cudaMalloc((void**)&deviceOutput, imageWidth*imageHeight*imageChannels*sizeof(float));
+  cudaMalloc((void**)&deviceHisto, HISTOGRAM_LENGTH*sizeof(float));
+  cudaMalloc((void**)&deviceCDF, HISTOGRAM_LENGTH*sizeof(float));
+
+  //copy input image to device
+  cudaMemcpy(deviceInput, hostInputImageData, imageWidth*imageHeight*imageChannels*sizeof(float), cudaMemcpyHostToDevice);
+
+  //set up grid dimensions
+  dim3 DimGrid1(ceil((float)imageWidth/BLOCK_SIZE),ceil((float)imageHeight/BLOCK_SIZE),1);
+  dim3 DimBlock1(BLOCK_WIDTH,BLOCK_WIDTH,1);
+
+  dim3 DimGrid2(1,1,1);
+  dim3 DimBlock2(HISTOGRAM_LENGTH/2,HISTOGRAM_LENGTH/2,1);
+
+  //call kernel
+  histEq1<<<DimGrid1, DimBlock1>>>();
+  computeCDF<<<DimGrid2, DimBlock2>>>(deviceHisto, deviceCDF, HISTOGRAM_LENGTH);
+
+  //retrieve output from device
+  cudaMemcpy(hostOutputImageData, deviceOutput, imageWidth*imageHeight*sizeof(float), cudaMemcpyHostToDevice);
 
   wbSolution(args, outputImage);
 
