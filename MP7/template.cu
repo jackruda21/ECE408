354da130692e8b5edc6e8b1ec8e4f67d2f89ffba
jackruda21 <jcruda2@illinois.edu>
2023-04-17 18:18:17 -0500

    MP7 Done

--- MP7/template.cu
+++ MP7/template.cu
@@ -21,6 +21,7 @@ __global__ void spmvJDSKernel(float *out, int *matColStart, int *matCols,
     float dot = 0;
     int num_in_row = matRows[row];
     int col_start = 0;
+    int num_count = 0;
     //for (int i = 0; i< num_in_row; i++) {
       //matColStart[i] would be the column in which i is in
       //but maybe you don't have an el in every column, sou you'd
@@ -39,11 +40,12 @@ __global__ void spmvJDSKernel(float *out, int *matColStart, int *matCols,
 
       //B
       //works equally as well as C
+      /*
       for(int i=0; i<matRows[0]; i++){
         if(matColStart[i] + row < matColStart[i+1]){
           dot += matData[matColStart[i]+row] * vec[matCols[matColStart[i]+row]];  
         }
-      }
+      }*/
 
 
       //C
@@ -54,6 +56,25 @@ __global__ void spmvJDSKernel(float *out, int *matColStart, int *matCols,
         col_start ++;
       }*/
     //}
+      
+    //after many failed attempts, THIS loop works
+    //what went wrong: logic was correct, but when we incremented 1 past the end of matColStart,
+    //our logic would no longer hold b/c it would be reading (assumadly) 0 for matColStart[end + 1]
+    //now we know that if there are still more row elements, and that we are in the last section of 
+    //matColStart, then we KNOW that this is the last element and that we won't be going past
+    //matColStart[col_start+1] since there is no matColStart[col_start+1]
+    //So if col_start = max index of matColStart, auto-pass check. matRows[0] is the row w/ 
+    //the larges # elements, so that -1 is max index of matColStart
+    while(num_count < num_in_row){
+      if(matColStart[col_start] + row < matColStart[col_start+1] || col_start == matRows[0]-1){
+        dot += matData[matColStart[col_start]+row] * vec[matCols[matColStart[col_start]+row]];
+        num_count ++;
+      }
+      col_start ++;
+      if(col_start == 1000){
+        break;
+      }
+    }
     out[matRowPerm[row]] = dot;
   }
 }
@@ -152,11 +173,39 @@ int main(int argc, char **argv) {
 
   wbTime_stop(GPU, "Freeing GPU Memory");
 
+  //DEBUGGING CODE
+  /*
   if(dim == 16){
+    wbLog(TRACE, "JDSColStart:");
+    for(int i=0; i<maxRowNNZ; i++){
+      wbLog(TRACE, hostJDSColStart[i]);
+    }
+
+    wbLog(TRACE, "JDSCols:");
+    for(int i=0; i<ndata; i++){
+      wbLog(TRACE, hostJDSCols[i]);
+    }
+
+    wbLog(TRACE, "JDSRowPerm:");
+    for(int i=0; i<dim; i++){
+      wbLog(TRACE, hostJDSRowPerm[i]);
+    }
+
+    wbLog(TRACE, "JDSRows:");
+    for(int i=0; i<dim; i++){
+      wbLog(TRACE, hostJDSRows[i]);
+    }
+
+    wbLog(TRACE, "JDSData:");
+    for(int i=0; i<ndata; i++){
+      wbLog(TRACE, hostJDSData[i]);
+    }
+
+    wbLog(TRACE, "Output:");
     for(int i=0; i<dim; i++){
       wbLog(TRACE, hostOutput[i]);
     }
-  }
+  }*/
 
   wbSolution(args, hostOutput, dim);
 
