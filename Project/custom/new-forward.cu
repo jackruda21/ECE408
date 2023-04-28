45984a99a5bceb44263f3d200e6f7d8612c7448c
jackruda21 <jcruda2@illinois.edu>
2023-04-28 04:50:42 -0500

    Done!

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -23,8 +23,70 @@
 int err_ct = 0;
 __constant__ float KFILTER[1 * 7 * 7 * 4*16];
 
-// H x W: 86 x 86, C: 1
-__global__ void conv_forward_kernel(float* __restrict__ output, const /*__half* */ float* __restrict__ input, const float* __restrict__ mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
+__global__ void conv_forward_kernel(float* __restrict__ output, const float* __restrict__ input, const float* __restrict__ mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
+{
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct AND fast.
+
+    Function paramter definitions:
+    output - output
+    input - input
+    mask - convolution kernel
+    Batch - batch_size (number of images in x)
+    Map_out - number of output feature maps
+    Channel - number of input feature maps
+    Height - input height dimension
+    Width - input width dimension
+    K - kernel height and width (K x K)
+    */
+
+    const int Height_out = Height - K + 1;
+    const int Width_out = Width - K + 1;
+    // (void)Height_out; // silence declared but never referenced warning. remove this line when you start working
+    // (void)Width_out; // silence declared but never referenced warning. remove this line when you start working
+
+    // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+    // An example use of these macros:
+    // float a = in_4d(0,0,0,0)
+    // out_4d(0,0,0,0) = a
+
+    #define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
+    #define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
+    #define mask_4d(i3, i2, i1, i0) KFILTER[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    // Insert your GPU convolution kernel code here
+    int W_grid = ceil(1.0*Width_out/TILE_WIDTH);
+    //int H_grid = ceil(1.0*Width_out/TILE_WIDTH);
+
+    int b, m, h, w;
+    b = blockIdx.z;
+    m = blockIdx.x;
+    h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
+    w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;
+    float acc = 0.0;
+    for (int c = 0; c < Channel; c++) { // sum over all input channels
+        if(h< Height-K+1 && w < Width-K+1){
+            #pragma unroll
+            for (int p = 0; p < K_SIZE; p++){ // loop over KxK filter, unroll convolution
+                for (int q = 0; q < K_SIZE; q++){
+                        acc += in_4d(b, c, h+p, w+q) * mask_4d(m, c, p, q);
+                }
+            }
+        }
+    }
+    if(h<Height_out && w<Width_out){
+        out_4d(b,m,h,w) = acc;
+    }
+    
+    
+    #undef out_4d
+    #undef in_4d
+    #undef mask_4d
+}
+
+__global__ void conv_forward_kernel_tiled(float* __restrict__ output, const /*__half* */ float* __restrict__ input, const float* __restrict__ mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
 {
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -44,7 +106,7 @@ __global__ void conv_forward_kernel(float* __restrict__ output, const /*__half* 
     */
 
     //set up shared mem tile, Assume K is always 7, and that channel <= 4 
-    __shared__ /*__half */ float input_tile[(TILE_HEIGHT+7-1)*(TILE_WIDTH+7-1)*4];
+    __shared__ float input_tile[(TILE_HEIGHT+7-1)*(TILE_WIDTH+7-1)*4];
 
     const int Height_out = Height - K + 1;
     const int Width_out = Width - K + 1;
@@ -139,6 +201,49 @@ __global__ void conv_forward_kernel(float* __restrict__ output, const /*__half* 
     #undef mask_4d
 }
 
+__global__ void conv_forward_kernel_FP16(float* __restrict__ output, const __half* __restrict__ input, const float* __restrict__ mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
+{
+    const int Height_out = Height - K + 1;
+    const int Width_out = Width - K + 1;
+    
+    // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+    // An example use of these macros:
+    // float a = in_4d(0,0,0,0)
+    // out_4d(0,0,0,0) = a
+
+    #define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
+    #define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
+    #define mask_4d(i3, i2, i1, i0) KFILTER[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    // Insert your GPU convolution kernel code here
+    int W_grid = ceil(1.0*Width_out/TILE_WIDTH);
+    //int H_grid = ceil(1.0*Width_out/TILE_WIDTH);
+
+    int b, m;//, h, w;
+    b = blockIdx.z;
+    m = blockIdx.x;
+    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
+    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;
+    __half acc = 0.0;
+    for (int c = 0; c < Channel; c++) { // sum over all input channels
+        if(h< Height-K+1 && w < Width-K+1){
+            #pragma unroll
+            for (int p = 0; p < K_SIZE; p++){ // loop over KxK filter, unroll convolution
+                for (int q = 0; q < K_SIZE; q++){
+                    acc = __hadd(acc, __hmul(in_4d(b, c, h+p, w+q), __float2half(mask_4d(m, c, p, q))));
+                }
+            }
+        }
+    }
+    if(h<Height_out && w<Width_out){
+        out_4d(b,m,h,w) = __half2float(acc);
+    }
+    
+    #undef out_4d
+    #undef in_4d
+    #undef mask_4d
+}
+
 #define CONVERT_SIZE 1024
 
 __global__ void convert_to_half(const float* input, __half* output, const int size){
@@ -210,8 +315,8 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
     cudaMalloc((void**)&device_half_input, input_size * sizeof(__half));
     dim3 blockConv(CONVERT_SIZE,1,1);
     dim3 gridConv(ceil(1.0*input_size/CONVERT_SIZE),1,1);
-    convert_to_half<<<gridConv, blockConv>>>(device_input, device_half_input, input_size);
-    */
+    convert_to_half<<<gridConv, blockConv>>>(device_input, device_half_input, input_size);*/
+    
 
     const int Height_out = Height - K + 1;
     const int Width_out = Width - K + 1;
@@ -226,7 +331,7 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
     // std::cout<<"Block Size: "<<(TILE_WIDTH+K-1)*(TILE_WIDTH+K-1)<<'\n';
 
     //FP16
-    /*conv_forward_kernel<<<gridDim, blockDim >>>(device_output, device_half_input, device_mask, Batch, Map_out, Channel, Height, Width, K);
+    /*conv_forward_kernel_FP16<<<gridDim, blockDim >>>(device_output, device_half_input, device_mask, Batch, Map_out, Channel, Height, Width, K);
     cudaDeviceSynchronize();
 
     cudaFree(device_half_input);*/
